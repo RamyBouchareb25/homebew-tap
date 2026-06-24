055c5c4046f7fd8d3d4d8387108efb2c1d923815
Ramybouchareb25 <rami.bouchareb@etu.usthb.dz>
2026-06-24 11:43:04 +0100

    Limit orbcert tap tests to macOS

--- Formula/orbcert.rb
+++ Formula/orbcert.rb
@@ -5,6 +5,7 @@ class Orbcert < Formula
   sha256 "99e53297836c36e35efd10c78da658eca86d165179364e8c8426fba808fff9bc"
   license "MIT"
 
+  depends_on :macos
   depends_on "go" => :build
   depends_on "kubernetes-cli"
 
